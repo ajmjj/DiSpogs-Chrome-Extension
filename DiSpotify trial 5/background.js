6cd748fef89ea4fdbe52dfa9d1b73f3a03cfa3dc
Anton Jurgens <antonj@me.com>
2022-10-09 19:52:04 +0200

    add user_signed_in checks

--- DiSpotify trial 5/background.js
+++ DiSpotify trial 5/background.js
@@ -6,6 +6,7 @@ const SHOW_DIALOG = encodeURIComponent('true');
 let STATE = '';
 let ACCESS_TOKEN = '';
 
+let user_signed_in = false;
 
 console.log(chrome.identity.getRedirectURL('spotify'));
 const getAuthUrl = function () {
@@ -20,38 +21,42 @@ const getAuthUrl = function () {
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     if (request.message === 'login') {
-        console.log('login message received');
-        chrome.identity.launchWebAuthFlow({
-            url: getAuthUrl(),
-            interactive: true
-        }, function (redirectUrl) {
-            console.log(`redirectUrl: ${redirectUrl}`);
-            // if (chrome.runtime.lastError) {
-            //     console.log('failed: chrome.runtime.lastError');
-            //     sendResponse({ message: 'fail' });
-            ACCESS_TOKEN = redirectUrl.substring(redirectUrl.indexOf('access_token=') + 13);
-            ACCESS_TOKEN = ACCESS_TOKEN.substring(0, ACCESS_TOKEN.indexOf('&'));
-            let state = redirectUrl.substring(redirectUrl.indexOf('state=') + 6);
+        if (user_signed_in) {
+            console.log("User is already signed in.");
+        } else {
+            console.log('login message received');
+            chrome.identity.launchWebAuthFlow({
+                url: getAuthUrl(),
+                interactive: true
+            }, function (redirectUrl) {
+                console.log(`redirectUrl: ${redirectUrl}`);
+                // if (chrome.runtime.lastError) {
+                //     console.log('failed: chrome.runtime.lastError');
+                //     sendResponse({ message: 'fail' });
+                ACCESS_TOKEN = redirectUrl.substring(redirectUrl.indexOf('access_token=') + 13);
+                ACCESS_TOKEN = ACCESS_TOKEN.substring(0, ACCESS_TOKEN.indexOf('&'));
+                let state = redirectUrl.substring(redirectUrl.indexOf('state=') + 6);
 
-            if (state === STATE){
-                console.log("SUCCESS")
-                user_signed_in = true;
+                if (state === STATE){
+                    console.log("SUCCESS")
+                    user_signed_in = true;
 
-                setTimeout(() => {
-                    ACCESS_TOKEN = '';
-                    chrome.identity.clearAllCachedAuthTokens();
-                    user_signed_in = false;
-                }, 3600000);
+                    setTimeout(() => {
+                        ACCESS_TOKEN = '';
+                        chrome.identity.clearAllCachedAuthTokens();
+                        user_signed_in = false;
+                    }, 3600000);
 
-                chrome.action.setPopup({ popup: './popup-signed-in.html' }, () => {
-                    sendResponse({ message: 'success' });
-                });
-            } else {
-                console.log('failed: state !== STATE');
-                sendResponse({ message: 'fail' });
-            }
-        });
-        return true;
+                    chrome.action.setPopup({ popup: './popup-signed-in.html' }, () => {
+                        sendResponse({ message: 'success' });
+                    });
+                } else {
+                    console.log('failed: state !== STATE');
+                    sendResponse({ message: 'fail' });
+                }
+            });
+            return true;
+        }
     };
 
     if (request.message === 'logout') {
